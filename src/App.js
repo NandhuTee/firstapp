a73f91072767ce453415ef0f64a9049171d7844a
Nandhini_T <rtnandhutee@gmail.com>
2024-02-14 09:10:26 +0530

    updatecommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css'; // Link to your CSS file
 
-function App() {
+export function App() {
+  const cards = [
+  {header: "title1", body: "body1", footer: "subscript1", isFooter: false},
+  {header: "title2", body: "body2", footer: "subscript2"},
+
+  {header: "title3", body: "body3", footer: "subscript3"},
+
+  {header: "title4", body: "body4", footer: "subscript4", isFooter: false},
+
+]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card-body" style={{display: "flex", gap: "20px"}}>
+    {
+      cards.map((card) => (
+        <div className= "card" style={{display: "flex", border: "2px black solid"}}>
+          <div>{card.header}</div>
+          <div>{card.body}</div>
+          {card.isFooter === false ? "" : card.footer}
+        </div>
+      ))
+    }
     </div>
-  );
+  )
 }
-
 export default App;
